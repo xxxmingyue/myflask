1b9e284c811e0b0d4a92c03984bcba16fba16a60
Mingyue1939 <Mingyue1939@outlook.com>
2021-02-02 21:21:19 +0800

    添加了自定义命令

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
+import click
+
 @app.route("/greet", defaults={'name':'programmer'})
 @app.route("/greet/<name>")
 def index(name):
     return "<h1>hello world! %s</h1>"%name
+
+
+@app.cli.command()
+def hello():
+    '''输出Hello ''' #用于显示帮助文档
+    click.echo("Hello, Human!")
     
