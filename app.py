773cce1d2cdd93ee43629fcf43b5327b0da84016
Mingyue1939 <Mingyue1939@outlook.com>
2021-02-03 17:30:48 +0800

    添加了set_cookie

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,abort
+from flask import Flask,request,abort, make_response
 app = Flask(__name__)
 import click
 
@@ -8,6 +8,11 @@ def index(name):
     num = request.args.get('num', '999gi')
     return "<h1>hello world! %s  No.%s</h1>"%(name,num)
 
+@app.route('/set/<name>')
+def set_cookie():
+    response = make_response(redirect(url_for(index)))
+    response.set_cookie('name', name)
+    return response
 
 @app.cli.command()
 def hello():
@@ -16,4 +21,6 @@ def hello():
 
 @app.route('/404')
 def not_found():
-    abort(404)
+    abort(404)   
+    '''abort()函数前不需要使用return语句，但一旦abort()函数被调用，abort()函数之后的代码将不会被执行。
+'''
