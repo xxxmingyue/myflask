bacc3b619dab8541808d5624114b57bbae80b814
Mingyue1939 <Mingyue1939@outlook.com>
2021-02-03 11:57:11 +0800

    添加了request请求

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask,request
 app = Flask(__name__)
 import click
 
 @app.route("/greet", defaults={'name':'programmer'})
 @app.route("/greet/<name>")
 def index(name):
-    return "<h1>hello world! %s</h1>"%name
+    num = request.args.get('num', '999')
+    return "<h1>hello world! %s  No.%s</h1>"%(name,num)
 
 
 @app.cli.command()
